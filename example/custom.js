4fbc183236decf5055dbda6c7a8715452fbcab1b
vinhnhan1002 <vinhnhan1002@gmail.com>
2023-03-29 18:12:36 +0700

    only latin charactor

--- example/custom.js
+++ example/custom.js
@@ -13,17 +13,20 @@ const schema = (input) =>
                         .minOfUppercase(5)
                         .minOfNumeric(6)
                         .noWhiteSpaces()
+                        .onlyLatinCharacters()
                         .messages({
-                            'password.minOfUppercase': '{#label} should contain at least {#min} uppercase character',
-                            'password.minOfSpecialCharacters': '{#label} should contain at least {#min} special character',
-                            'password.minOfLowercase': '{#label} should contain at least {#min} lowercase character',
-                            'password.minOfNumeric': '{#label} should contain at least {#min} numeric character',
-                            'password.noWhiteSpaces': '{#label} should not contain white spaces',
+                              'password.minOfUppercase': '{#label} should contain at least {#min} uppercase character',
+                              'password.minOfSpecialCharacters':
+                                    '{#label} should contain at least {#min} special character',
+                              'password.minOfLowercase': '{#label} should contain at least {#min} lowercase character',
+                              'password.minOfNumeric': '{#label} should contain at least {#min} numeric character',
+                              'password.noWhiteSpaces': '{#label} should not contain white spaces',
+                              'password.onlyLatinCharacters': '{#label} should contain only latin characters',
                         }),
             })
             .validate(input, { abortEarly: false });
 
-const { error } = schema({ username: 'aA', password:  'aA@0 ' });
+const { error } = schema({ username: 'aA', password: 'aA@0 ' });
 
 console.log(error);
 // 'password' should contain at least 3 special character
@@ -31,4 +34,4 @@ console.log(error);
 // 'password' should contain at least 5 uppercase character
 // 'password'  should contain at least 6 numeric character
 // 'password' should not contain white spaces
-
+// 'password' should contain only latin characters
